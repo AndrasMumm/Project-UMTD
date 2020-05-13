bf59379a7a68d19326473a03e4091a3f34ad1155
LennardR <lennard.rohs@gmail.com>
2020-05-14 00:44:42 +0200

    main to main

--- Project UMTD/Main.cpp
+++ Project UMTD/Main.cpp
@@ -2,7 +2,35 @@
 #include <windows.h>
 #include <Rendering/renderingMain.h>
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow)
+#include "Rendering\systemclass.h"
+
+#include <dxgi.h>
+#include <d3dcommon.h>
+#include <d3d11.h>
+
+int WINAPI RenderingMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow)
 {
-	RenderingMain(hInstance, hPrevInstance, pScmdline, iCmdshow);
+	SystemClass* System;
+	bool result;
+
+	// Create the system object.
+	System = new SystemClass();
+	if (!System)
+	{
+		return 0;
+	}
+
+	// Initialize and run the system object.
+	result = System->Initialize();
+	if (result)
+	{
+		System->Run();
+	}
+
+	// Shutdown and release the system object.
+	System->Shutdown();
+	delete System;
+	System = 0;
+
+	return 0;
 }
