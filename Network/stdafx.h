3f6831cbc6e11544154b9237b1e520de031330ac
Magxm <19425588+Magxm@users.noreply.github.com>
2020-05-13 23:30:54 +0200

    Added Boost and dependency folder to all projects

--- Network/stdafx.h
+++ Network/stdafx.h
@@ -1,4 +1,4 @@
-// stdafx.h : include file for standard system include files,
+﻿// stdafx.h : include file for standard system include files,
 // or project specific include files that are used frequently, but
 // are changed infrequently
 //
@@ -9,6 +9,7 @@
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 
-
-
 // reference additional headers your program requires here
+#define NOMINMAX
+#include <Windows.h>
+#include <boost/asio.hpp>
