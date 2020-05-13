d9ae38d436e23d7be7cf88af143b8e01947c2a22
Tom <tom.schaefers@rwth-aachen.de>
2020-05-14 00:35:36 +0200

    Initial Rendering Commit + Project changes

---
+++ Rendering/renderingMain.h
@@ -0,0 +1,37 @@
+#include "systemclass.h"
+
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "d3dx11.lib")
+#pragma comment(lib, "d3dx10.lib")
+
+#include <dxgi.h>
+#include <d3dcommon.h>
+#include <d3d11.h>
+
+int WINAPI RenderingMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow)
+{
+	SystemClass* System;
+	bool result;
+
+	// Create the system object.
+	System = new SystemClass();
+	if (!System)
+	{
+		return 0;
+	}
+
+	// Initialize and run the system object.
+	result = System->Initialize();
+	if (result)
+	{
+		System->Run();
+	}
+
+	// Shutdown and release the system object.
+	System->Shutdown();
+	delete System;
+	System = 0;
+
+	return 0;
+}
