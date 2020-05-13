adab621b215709435a259f18f3dccb273b8ab41f
Tom <tom.schaefers@rwth-aachen.de>
2020-05-13 23:18:09 +0200

    beginning of Systemclass.cpp

--- Rendering/stdafx.h
+++ Rendering/stdafx.h
@@ -9,6 +9,12 @@
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "d3dx11.lib")
+#pragma comment(lib, "d3dx10.lib")
 
-
+#include <dxgi.h>
+#include <d3dcommon.h>
+#include <d3d11.h>
 // reference additional headers your program requires here
---
+++ Rendering/systemclass.cpp
@@ -0,0 +1,74 @@
+#include "systemclass.h"
+
+SystemClass::SystemClass() {
+	m_Input = 0;
+	m_Graphics = 0;
+}
+
+SystemClass::SystemClass(const SystemClass& other) {
+}
+
+SystemClass::~SystemClass() {
+}
+
+bool SystemClass::Initialize()
+{
+	int screenWidth, screenHeight;
+	bool result;
+
+	// Initialize the width and height of the screen to zero before sending the variables into the function.
+	screenWidth = 0;
+	screenHeight = 0;
+
+	// Initialize the windows api.
+	InitializeWindows(screenWidth, screenHeight);
+
+	// Create the input object.  This object will be used to handle reading the keyboard input from the user.
+	m_Input = new InputClass;
+	if (!m_Input)
+	{
+		return false;
+	}
+
+	// Initialize the input object.
+	m_Input->Initialize();
+
+	// Create the graphics object.  This object will handle rendering all the graphics for this application.
+	m_Graphics = new GraphicsClass;
+	if (!m_Graphics)
+	{
+		return false;
+	}
+
+	// Initialize the graphics object.
+	result = m_Graphics->Initialize(screenWidth, screenHeight, m_hwnd);
+	if (!result)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void SystemClass::Shutdown()
+{
+	// Release the graphics object.
+	if (m_Graphics)
+	{
+		m_Graphics->Shutdown();
+		delete m_Graphics;
+		m_Graphics = 0;
+	}
+
+	// Release the input object.
+	if (m_Input)
+	{
+		delete m_Input;
+		m_Input = 0;
+	}
+
+	// Shutdown the window.
+	ShutdownWindows();
+
+	return;
+}
