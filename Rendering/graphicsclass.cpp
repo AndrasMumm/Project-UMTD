393c283176e9fc450488c73fefa105cc06487eee
Tom <tom.schaefers@rwth-aachen.de>
2020-10-01 00:13:11 +0200

    added texture

--- Rendering/graphicsclass.cpp
+++ Rendering/graphicsclass.cpp
@@ -61,10 +61,9 @@ bool GraphicsClass::Initialize(int screenWidth, int screenHeight, HWND hwnd)
 
 	// Initialize the model object.
 	
-	result = m_Model->Initialize(m_Direct3D->GetDevice(), m_Direct3D->GetDeviceContext(), "..\..\..\stone01.tga");
+	result = m_Model->Initialize(m_Direct3D->GetDevice(), m_Direct3D->GetDeviceContext(), "../stone01.tga");
 	if (!result)
 	{
-		OutputDebugStringW(L"start");
 		MessageBox(hwnd, L"Could not initialize the model object.", L"Error", MB_OK);
 		return false;
 	}
