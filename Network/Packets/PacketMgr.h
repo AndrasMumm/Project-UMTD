511ec18221952645ed5806375163ed46bc4e21da
Magxm <19425588+Magxm@users.noreply.github.com>
2020-10-01 02:25:31 +0200

    Beginning of a PacketMgr

---
+++ Network/Packets/PacketMgr.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "packet.h"
+
+class PacketMgr
+{
+public:
+	PacketMgr& getInstance()
+	{
+		static PacketMgr instance;
+		return instance;
+	}
+
+
+private:
+	PacketMgr() {}
+
+public:
+	void send(Packet& packet, int recipient = -1);
+	void handle(Packet& packet, int sender);
+};
